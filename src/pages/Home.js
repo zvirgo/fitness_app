110cf7e096e7980142037ed8619596ef2d229f4d
zvirgo <zeinali_bahareh@yahoo.com>
2023-06-07 11:27:10 +0330

    create HorizontalScrollbar component for scroll BodyPart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,21 @@ import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+       />
     </Box>
   );
 };
